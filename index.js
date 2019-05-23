7f1b9f7d96dc2938f9319e2207c52b623c55acd9
Vuk Perovic <perovicv515@gmail.com>
2019-05-23 22:22:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,12 +10,7 @@
 
 
 function johnLennonFacts () {
-  const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
+  const facts = []
 const newFacts = []
 var i = 0;
 while (i < facts.length)  {
