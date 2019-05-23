8f119f701bcd17c1b7b7206661072629724d1df5
Vuk Perovic <perovicv515@gmail.com>
2019-05-23 22:07:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function johnLennonFacts () {
   "He was a choir boy and boy scout",
   "He hated the sound of his own voice"
 ];
-var newFacts = []
+const newFacts = []
 var i=0;
 while(i<facts.length)  {
   newFacts.push(facts[i] + "!!!");
