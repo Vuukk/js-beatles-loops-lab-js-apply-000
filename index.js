1185b42fc8e03c5d8c9f0b75de56a1ca94017178
Vuk Perovic <perovicv515@gmail.com>
2019-05-23 21:54:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
   function theBeatlesPlay (musicians, instruments) {
     var allMusicians = []
     for (let i=0; i<musicians.length; i++) {
-      allPlayers.push()
+      allPlayers.push(musicians[i] + instruments[i])
     }
   }
